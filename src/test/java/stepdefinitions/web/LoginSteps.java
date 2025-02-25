83f44143aba2fd75889ff321a43c42d2225596d5
aryo <bharataaryaseta@gmail.com>
2025-02-25 22:24:45 +0700

    Fix loginsteps

--- src/test/java/stepdefinitions/web/LoginSteps.java
+++ src/test/java/stepdefinitions/web/LoginSteps.java
@@ -1,33 +1,37 @@
 package stepdefinitions.web;
 
 import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginSteps {
     WebDriver driver;
 
     @Given("User is on the login page")
     public void userIsOnLoginPage() {
-        // Set path ke ChromeDriver (pastikan path ini sesuai dengan lokasi file di komputermu)
-        System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://www.demoblaze.com/");
     }
 
     @When("User enters valid username and password")
     public void userEntersValidCredentials() {
-        // Implement login logic here
+        driver.findElement(By.id("loginusername")).sendKeys("testuser");
+        driver.findElement(By.id("loginpassword")).sendKeys("password123");
     }
 
     @And("User clicks on login button")
     public void userClicksLoginButton() {
-        // Implement button click here
+        driver.findElement(By.xpath("//button[contains(text(),'Log in')]")).click();
     }
 
     @Then("User should be redirected to homepage")
     public void userRedirectedToHomepage() {
-        // Implement assertion here
+        WebElement logoutButton = driver.findElement(By.id("logout2"));
+        assert logoutButton.isDisplayed();
+        driver.quit();
     }
 }
